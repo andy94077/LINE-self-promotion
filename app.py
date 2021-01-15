c5c2fbb3b56356e2c894589629087aefad775661
andy94077 <andy94077@gmail.com>
2021-01-15 17:12:22 +0800

    Add debug message

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,6 +17,23 @@ line_bot_api = LineBotApi('fiK5JVpGVGIM2CGc4+1CSTLeVupHBdGFW+/Iddix9zusohj4Wxnpb
 # Channel Secret
 handler = WebhookHandler('351967b479cda699b4bc98225a253054')
 
+reply_msgs = {
+    'work history': '''CMLab, National Taiwan University. Special Research Undergraduate, Jan. 2020 – Present.
+Funpodium. Web Developer Intern, July 2020 – Aug. 2020.
+AICS, ASUSTek COMPUTER INC. Software Engineering Intern, July 2019 – Aug. 2019.
+''',
+    'skills': 'C, C++, Python, Machine Learning, Deep Learning, React, Javascript',
+    'awards': '''Honorable Mention, Special Research Exhibition – Department of Computer Science, National Taiwan University, June 2020.
+Student Group Leaderboards Honorable Mention, Ministry of Education Intercollegiate AI CUP 2019 – Ministry of Education, Jan. 2020.
+''',
+    'help': u'''I am Andy Chen.
+I'm a senior student at the Department of Computer Science, National Taiwan University. I specialize in frontend, backend, and deep learning. Type 
+
+• "Work History" to show my working experience.
+• "Skills" to show my programming skills.
+• "Awards" to show awards I got.
+Or, type "Help" to show this helping message.'''
+}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -36,24 +54,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text.lower()
-    if text == 'work history':
-        reply = '''CMLab, National Taiwan University. Special Research Undergraduate, Jan. 2020 – Present.
-Funpodium. Web Developer Intern, July 2020 – Aug. 2020.
-AICS, ASUSTek COMPUTER INC. Software Engineering Intern, July 2019 – Aug. 2019.
-'''
-    elif text == 'skills':
-        reply = 'C, C++, Python, Machine Learning, Deep Learning, React, Javascript'
-    elif text == 'awards':
-        reply = '''Honorable Mention, Special Research Exhibition – Department of Computer Science, National Taiwan University, June 2020.
-Student Group Leaderboards Honorable Mention, Ministry of Education Intercollegiate AI CUP 2019 – Ministry of Education, Jan. 2020.
-'''
-    else:
-        reply = u'''I am Yi-Rong Chen.
-I'm a senior student at the Department of Computer Science, National Taiwan University. I specialize in frontend, backend, and deep learning. Type 
-• "Work History" to show my working experience.
-• "Skills" to show my programming skills.
-• "Awards" to show awards I got.
-Or, type "Help" to show this helping message.'''
+    app.logger.info(text)
+    reply = reply_msgs.get(text, reply_msgs['help'])
 
     message = TextSendMessage(text=reply)
     line_bot_api.reply_message(event.reply_token, message)
@@ -61,4 +63,6 @@ Or, type "Help" to show this helping message.'''
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    handler = logging.FileHandler('flask.log')
+    app.logger.addHandler(handler)
     app.run(host='0.0.0.0', port=port)
